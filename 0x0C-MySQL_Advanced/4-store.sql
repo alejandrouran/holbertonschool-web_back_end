dce2b910ed0cef17e019d25fe0e89de2a4754ff1
alejuran <alejandrourn@gmail.com>
2023-03-09 15:01:26 -0500

    correction four and seven task

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
 -- creates a trigger that decreases
-CREATE TRIGGER 'order_decrease BEFORE INSERT ON	orders'
+CREATE TRIGGER order_decrease BEFORE INSERT ON orders
 FOR EACH ROW UPDATE items
 SET quantity = quantity - NEW.number
 WHERE name = NEW.item_name;
--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,10 +1,11 @@
 -- creates a stored procedure
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER |
-CREATE PROCEDURE ComputeAverageScoreForUser (
-	IN user_id INT,
+CREATE PROCEDURE ComputeAverageScoreForUser(
+  IN user_id INT)
 BEGIN
-	DECLARE avg_score FLOAT;
-	SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
-	UPDATE users SET average_score = avg_score WHERE id=user_id;
+  DECLARE avg_score FLOAT;
+  SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+  UPDATE users SET average_score = avg_score WHERE id=user_id;
 END;
 |
