cebfe68ad973ea199d5b73a2b547dd153ffba42f
alejuran <alejandrourn@gmail.com>
2023-03-14 11:41:13 -0500

    nosql all

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function that changes all topics 
+of a school document based on the name: """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ will be the pymongo collection object """
+    return mongo_collection.update_many({ "name": name }, { "$set": { "topics": topics } })
---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+""" provides some stats about Nginx logs stored in MongoDB: """
